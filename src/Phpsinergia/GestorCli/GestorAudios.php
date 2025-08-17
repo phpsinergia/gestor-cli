295e12b8ed3e4f85c3256ce9e0d10e76a974d9b3
Ruben Araya Tagle <rubenarayatagle@gmail.com>
2025-08-17 03:42:16 -0400

    Actualizacion

--- src/Phpsinergia/GestorCli/GestorAudios.php
+++ src/Phpsinergia/GestorCli/GestorAudios.php
@@ -4,19 +4,21 @@ namespace Phpsinergia\GestorCli;
 
 final class GestorAudios extends Gestor
 {
+    private string $rutaFfmpeg;
+
     public function ejecutarComando(?array $entrada = null): int
     {
-        $parametrosValidos = ['carpeta', 'archivo', 'datos', 'nivel', 'duracion', 'bitrate', 'inicial'];
+        $parametrosValidos = ['destino', 'ar_wav', 'ar_txt', 'silencio', 'duracion', 'bitrate', 'listado', 'numero', 'formato'];
         if ($this->_procesarEntrada($entrada, $parametrosValidos) > 0) {
             $this->_ayuda();
             return 1;
         }
+        $this->rutaFfmpeg = $this->_leer('RUTA_FFMPEG');
         return match ($this->comando) {
-            'mapear_cortes' => $this->mapear_cortes(),
+            'detectar_cortes' => $this->detectar_cortes(),
             'dividir_audio' => $this->dividir_audio(),
-            'exportar_aac' => $this->exportar_aac(),
-            'agregar_tags' => $this->agregar_tags(),
-            'cambiar_nombres' => $this->cambiar_nombres(),
+            'convertir_audios' => $this->convertir_audios(),
+            'agregar_metadatos' => $this->agregar_metadatos(),
             'herramientas' => $this->herramientas(),
             'ayuda' => $this->_ayuda(),
             'version' => $this->_version(),
@@ -29,33 +31,79 @@ final class GestorAudios extends Gestor
     {
         $this->_mensaje('COMPROBANDO HERRAMIENTAS DEL GESTOR DE AUDIOS...', Mensajes::INFO);
         $herramientas = [
+            "{$this->rutaFfmpeg}ffmpeg" => 'FFmpeg',
         ];
-        $this->_verificarHerramientas($herramientas);
+        $this->_verificarHerramientas($herramientas, '-version');
         return $this->_mensaje('Comprobación de herramientas finalizada.', Mensajes::AVISO);
     }
-    private function mapear_cortes(): int
+
+    private function detectar_cortes(): int
     {
-        $this->_mensaje('...', Mensajes::INFO);
-        $carpeta = $this->_param('carpeta');
+        $this->_mensaje('DETECTANDO PUNTOS DE CORTE EN ARCHIVO DE AUDIO...', Mensajes::INFO);
+        $destino = $this->_param('destino');
+        $ar_wav = $this->_param('ar_wav');
+        $silencio = $this->_param('silencio');
+        $duracion = $this->_param('duracion');
+        $rutaOrigen = $this->_comprobarRuta($ar_wav);
+        if (!$this->_validarArchivo($rutaOrigen)) return 1;
+        $destino = $this->_comprobarRuta($destino, 'carpeta');
+        if (empty($destino)) return $this->_mensaje('Debes indicar una carpeta de destino existente (--destino=).', Mensajes::ERROR);
+        if (empty($silencio) || empty($duracion)) return $this->_mensaje('Debes indicar el nivel de silencio (--silencio=) y su duración mínima (--duracion=).', Mensajes::ERROR);
+        $nombreArchivo = pathinfo($ar_wav, PATHINFO_FILENAME);
+        $archivoCortes = "$destino/$nombreArchivo.txt";
+        $args = [
+            '-i',
+            $rutaOrigen,
+            '-af', 'silencedetect=n=-' . $silencio . 'dB:d=' . $duracion,
+            '-f', 'null', '-'
+        ];
+        $this->_ejecutarComandoSeguro("{$this->rutaFfmpeg}ffmpeg", $args, '2>', $archivoCortes);
+        if ($this->_validarArchivo($archivoCortes, false) === true)
+            return $this->_mensaje("Archivo de cortes guardado.\n$archivoCortes", Mensajes::EXITO);
+        return $this->_mensaje("No se pudo guardar el archivo de cortes: $archivoCortes", Mensajes::ERROR);
+    }
+
+    /*
+    - Archivo de origen (WAV)
+    - Carpeta de destino
+    - Archivo de datos
+    - Número inicial
+    ffmpeg -i xxx.wav -ss NN -to NN -c copy 01.wav
+    */
+    private function dividir_audio(): int
+    {
+        $this->_mensaje('DIVIDIENDO ARCHIVO DE AUDIO...', Mensajes::INFO);
+        $ar_wav = $this->_param('ar_wav');
+        $ar_txt = $this->_param('ar_txt');
+        $destino = $this->_param('destino');
+        $numero = $this->_param('numero');
 
         return $this->_mensaje("", Mensajes::EXITO);
     }
-    private function dividir_audio(): int
+
+    /*
+    - Formato de salida (AAC)
+    - Bitrate (576k)
+    - Carpeta de origen
+    for %f in (*.wav) do .\ffmpeg -i "%f" -c:a aac -b:a 576k -strict -2 "%~nf.m4a"
+    */
+    private function convertir_audios(): int
     {
-        return 0;
-    }
-    private function exportar_aac(): int
-    {
-        return 0;
-    }
-    private function agregar_tags(): int
-    {
-        return 0;
-    }
-    private function cambiar_nombres(): int
-    {
-        return 0;
+        $this->_mensaje('CONVIRTIENDO ARCHIVOS DE AUDIO...', Mensajes::INFO);
+        $origen = $this->_param('origen');
+        $formato = $this->_param('formato');
+        $bitrate = $this->_param('bitrate');
+
+        return $this->_mensaje("", Mensajes::EXITO);
     }
 
-    // FUNCIONES DE UTILIDAD
+    private function agregar_metadatos(): int
+    {
+        $this->_mensaje('AGREGANDO METADATOS A ARCHIVOS...', Mensajes::INFO);
+        $origen = $this->_param('origen');
+        $listado = $this->_param('listado');
+
+        return $this->_mensaje("", Mensajes::EXITO);
+    }
+
 }
